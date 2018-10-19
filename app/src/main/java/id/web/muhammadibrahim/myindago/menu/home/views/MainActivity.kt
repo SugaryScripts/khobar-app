637a5afee3993a1f348e58eeb3fa8f38dd572c63
Unknown <fryzexultant@gmail.com>
2018-10-19 15:47:08 +0700

    :art: :arrow_up: enabled data binding

--- app/src/main/java/id/web/muhammadibrahim/myindago/menu/home/views/MainActivity.kt
+++ app/src/main/java/id/web/muhammadibrahim/myindago/menu/home/views/MainActivity.kt
@@ -1,13 +1,23 @@
 package id.web.muhammadibrahim.myindago.menu.home.views
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import id.web.muhammadibrahim.myindago.R
+import id.web.muhammadibrahim.myindago.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBinding()
+    }
+
+    private fun setupBinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
