03ad0c9d3ab51ffa7d0b32ae8ea69f0a0a18cbd2
Unknown <fryzexultant@gmail.com>
2018-10-23 11:09:51 +0700

    :lipstick: :pencil: complete more click event on Home

--- app/src/main/java/id/web/muhammadibrahim/myindago/menu/main/views/activities/MoreEventActivity.kt
+++ app/src/main/java/id/web/muhammadibrahim/myindago/menu/main/views/activities/MoreEventActivity.kt
@@ -2,8 +2,8 @@ package id.web.muhammadibrahim.myindago.menu.main.views.activities
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import id.web.muhammadibrahim.myindagodev.R
+import android.support.v7.app.AppCompatActivity
+import id.web.muhammadibrahim.myindago.R
 
 import kotlinx.android.synthetic.main.activity_more_event.*
 
