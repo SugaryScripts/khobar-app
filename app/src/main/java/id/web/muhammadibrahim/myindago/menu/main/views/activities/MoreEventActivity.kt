7e8ed60d0ae986985a7c754402625e2838a23e08
Unknown <fryzexultant@gmail.com>
2018-10-23 15:09:39 +0700

    :newspaper: :sparkles: Added recycler for more event activity

--- app/src/main/java/id/web/muhammadibrahim/myindago/menu/main/views/activities/MoreEventActivity.kt
+++ app/src/main/java/id/web/muhammadibrahim/myindago/menu/main/views/activities/MoreEventActivity.kt
@@ -1,23 +1,57 @@
 package id.web.muhammadibrahim.myindago.menu.main.views.activities
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
 import id.web.muhammadibrahim.myindago.R
+import id.web.muhammadibrahim.myindago.databinding.ActivityMoreEventBinding
+import id.web.muhammadibrahim.myindago.menu.main.adapters.EventMoreAdapter
+import id.web.muhammadibrahim.myindago.menu.main.interfaces.EventClickListener
+import id.web.muhammadibrahim.myindago.menu.main.models.EventModel
+import id.web.muhammadibrahim.myindago.menu.main.viewmodels.FragmentHomeViewModel
 
-import kotlinx.android.synthetic.main.activity_more_event.*
+class MoreEventActivity : AppCompatActivity(), EventClickListener {
 
-class MoreEventActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMoreEventBinding
+    lateinit var viewModel: FragmentHomeViewModel
+
+    lateinit var adapterEvent: EventMoreAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_more_event)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        setupBinding()
+        setupToolbar()
+        setupRecycler()
+    }
+
+    fun setupBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_more_event)
+        viewModel = ViewModelProviders.of(this).get(FragmentHomeViewModel::class.java)
+    }
+
+    fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+    }
+
+    fun setupRecycler() {
+        adapterEvent = EventMoreAdapter(this)
+        viewModel.eventModel.observe(this, Observer {
+            adapterEvent.setEvents(it)
+        })
+        binding.contentMOREEVENT.rvMoreEvent.apply {
+            adapter = adapterEvent
+            layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+            itemAnimator = DefaultItemAnimator(); hasFixedSize()
         }
     }
 
+    override fun onClickEvent(eventModel: EventModel) {
+        Toast.makeText(this,"clicked ${eventModel.title}", Toast.LENGTH_SHORT).show()
+    }
 }
