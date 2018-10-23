0c832d62bbea22d53cabab5873e76efa4dde663b
Unknown <fryzexultant@gmail.com>
2018-10-23 10:41:30 +0700

    :tada: init basic activity more event from android studio IDE

---
+++ app/src/main/java/id/web/muhammadibrahim/myindago/menu/main/views/activities/MoreEventActivity.kt
@@ -0,0 +1,23 @@
+package id.web.muhammadibrahim.myindago.menu.main.views.activities
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import id.web.muhammadibrahim.myindagodev.R
+
+import kotlinx.android.synthetic.main.activity_more_event.*
+
+class MoreEventActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_event)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
