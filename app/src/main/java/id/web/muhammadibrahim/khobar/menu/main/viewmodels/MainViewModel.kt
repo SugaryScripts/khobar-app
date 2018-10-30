ea15ae5ec995515e691e648e0e84ac268005ee70
Unknown <fryzexultant@gmail.com>
2018-10-30 22:46:56 +0700

    :speaker: :construction: :pencil: load fragment

--- app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/viewmodels/MainViewModel.kt
+++ app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/viewmodels/MainViewModel.kt
@@ -2,6 +2,7 @@ package id.web.muhammadibrahim.khobar.menu.main.viewmodels
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
+import android.util.Log
 import id.web.muhammadibrahim.khobar.menu.main.views.fragments.HomeFragment
 
 class MainViewModel(private val fragmentManager: FragmentManager, private val frame_containter: Int) {
@@ -11,11 +12,16 @@ class MainViewModel(private val fragmentManager: FragmentManager, private val fr
     }
 
     fun loadFragment(fragment: Fragment) {
-        if (fragment != fragmentManager.findFragmentById(frame_containter)){
-            // load fragment
-            fragmentManager.beginTransaction()
-                .replace(frame_containter, fragment)
-                .commit()
+        Log.i("(^_^)","current Fragment = "+fragmentManager.findFragmentById(frame_containter).toString())
+        Log.i("(^_^)","input fragment = "+fragment.toString())
+        if (fragmentManager.findFragmentById(frame_containter) != fragment ){
+            // todo: fix multiple fragment
+            Log.i("(^_^)","current fragment and target is different so change!")
+                fragmentManager.beginTransaction()
+                    .replace(frame_containter, fragment)
+                    .commit()
+        }else {
+            Log.i("(^_^)","The fragment is not change!!")
         }
     }
 }
