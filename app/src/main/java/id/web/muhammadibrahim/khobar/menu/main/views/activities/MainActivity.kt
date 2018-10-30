2ca3b5ff6c8ad5d9c657ee4e1dc2baef5e1abc03
Unknown <fryzexultant@gmail.com>
2018-10-30 22:43:51 +0700

    :art: :pencil: :ambulance: Fix onBack pressed

--- app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/views/activities/MainActivity.kt
+++ app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/views/activities/MainActivity.kt
@@ -3,21 +3,29 @@ package id.web.muhammadibrahim.khobar.menu.main.views.activities
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.FragmentManager
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import id.web.muhammadibrahim.khobar.R
 import id.web.muhammadibrahim.khobar.databinding.ActivityMainBinding
 import id.web.muhammadibrahim.khobar.menu.main.interfaces.EventClickListener
 import id.web.muhammadibrahim.khobar.menu.main.interfaces.NewsClickListener
+import id.web.muhammadibrahim.khobar.menu.main.models.EventModel
+import id.web.muhammadibrahim.khobar.menu.main.models.NewsModel
 import id.web.muhammadibrahim.khobar.menu.main.viewmodels.MainViewModel
 import id.web.muhammadibrahim.khobar.menu.main.views.fragments.EventFragment
 import id.web.muhammadibrahim.khobar.menu.main.views.fragments.HomeFragment
 import id.web.muhammadibrahim.khobar.menu.main.views.fragments.NewsFragment
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity(), NewsClickListener, EventClickListener {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewmodel: MainViewModel
+    lateinit var viewmodel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -93,4 +101,13 @@ class MainActivity : AppCompatActivity(), NewsClickListener, EventClickListener 
         startActivity(DetailEventActivity.newIntent(this,eventModel))
     }
 
+    override fun onBackPressed() {
+        if (supportFragmentManager.findFragmentById(binding.flContentMAIN.id) is HomeFragment) {
+            Log.i("(^_^)", "On else so it's finish")
+            super.onBackPressed()
+        } else {
+            Log.i("(^_^)", "It is home fragment so it still alive")
+            binding.navigation.selectedItemId = R.id.navigation_home
+        }
+    }
 }
