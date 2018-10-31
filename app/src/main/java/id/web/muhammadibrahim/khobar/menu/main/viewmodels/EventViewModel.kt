cb58e5537db6619295ca75ab05ac624c00ca2418
Unknown <fryzexultant@gmail.com>
2018-10-31 18:39:54 +0700

    :fire: Remove unused viewmodel

--- app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/viewmodels/EventViewModel.kt
+++
@@ -1,7 +0,0 @@
-package id.web.muhammadibrahim.khobar.menu.main.viewmodels
-
-import android.arch.lifecycle.ViewModel;
-
-class EventViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
