9d739ca867f8f61796afc0117f1363800a71285c
Unknown <fryzexultant@gmail.com>
2018-10-31 18:44:35 +0700

    :ambulance: :lipstick: Fix animation title toolbar detail news and event

--- app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/views/activities/DetailEventActivity.kt
+++ app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/views/activities/DetailEventActivity.kt
@@ -6,12 +6,17 @@ import android.os.Bundle
 import id.web.muhammadibrahim.khobar.R
 import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.support.design.widget.AppBarLayout
+import android.view.View
 import id.web.muhammadibrahim.khobar.databinding.ActivityDetailEventBinding
 import id.web.muhammadibrahim.khobar.menu.main.models.EventModel
 import id.web.muhammadibrahim.khobar.menu.main.viewmodels.ItemEventViewModel
+import android.view.animation.AlphaAnimation
 
 
-class DetailEventActivity : AppCompatActivity() {
+
+
+class DetailEventActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
 
     companion object {
         const val EXTRA_EVENT = "extra_event_id"
@@ -27,6 +32,10 @@ class DetailEventActivity : AppCompatActivity() {
     lateinit var viewModel: ItemEventViewModel
     lateinit var eventModel: EventModel
 
+    private val PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f
+    private val ALPHA_ANIMATIONS_DURATION: Long = 200
+    private var mIsTheTitleVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -40,5 +49,40 @@ class DetailEventActivity : AppCompatActivity() {
         }
         viewModel = ItemEventViewModel(eventModel)
         binding.detailEvent = viewModel
+        binding.titleToolbarDetailEvent.visibility = View.INVISIBLE
+        binding.appbarDetailEvent.addOnOffsetChangedListener(this)
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, offset: Int) {
+        val maxScroll = appBarLayout.getTotalScrollRange()
+        val percentage: Float = Math.abs(offset) / maxScroll.toFloat()
+        handleToolbarTitleVisibility(percentage)
+    }
+
+    private fun handleToolbarTitleVisibility(percentage: Float) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if (!mIsTheTitleVisible) {
+                        startAlphaAnimation(binding.titleToolbarDetailEvent, ALPHA_ANIMATIONS_DURATION, View.VISIBLE)
+                mIsTheTitleVisible = true
+            }
+
+        } else {
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(binding.titleToolbarDetailEvent, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE)
+                mIsTheTitleVisible = false
+            }
+        }
+    }
+
+    private fun startAlphaAnimation(v: View, duration: Long, visibility: Int) {
+        val alphaAnimation = if (visibility == View.VISIBLE)
+            AlphaAnimation(0f, 1f)
+        else
+            AlphaAnimation(1f, 0f)
+
+        alphaAnimation.duration = duration
+        alphaAnimation.fillAfter = true
+        v.startAnimation(alphaAnimation)
     }
 }
--- app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/views/activities/DetailNewsActivity.kt
+++ app/src/main/java/id/web/muhammadibrahim/khobar/menu/main/views/activities/DetailNewsActivity.kt
@@ -5,12 +5,15 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.view.View
+import android.view.animation.AlphaAnimation
 import id.web.muhammadibrahim.khobar.R
 import id.web.muhammadibrahim.khobar.databinding.ActivityDetailNewsBinding
 import id.web.muhammadibrahim.khobar.menu.main.models.NewsModel
 import id.web.muhammadibrahim.khobar.menu.main.viewmodels.ItemNewsViewModel
 
-class DetailNewsActivity : AppCompatActivity() {
+class DetailNewsActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListener {
 
     companion object {
         const val EXTRA_NEWS = "extra_news"
@@ -26,6 +29,10 @@ class DetailNewsActivity : AppCompatActivity() {
     lateinit var viewModel: ItemNewsViewModel
     lateinit var newsModel: NewsModel
 
+    private val PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f
+    private val ALPHA_ANIMATIONS_DURATION: Long = 200
+    private var mIsTheTitleVisible = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,5 +46,40 @@ class DetailNewsActivity : AppCompatActivity() {
         }
         viewModel = ItemNewsViewModel(newsModel)
         binding.detailNews = viewModel
+        binding.titleToolbarDetailNews.visibility = View.INVISIBLE
+        binding.appbarDetailNews.addOnOffsetChangedListener(this)
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, offset: Int) {
+        val maxScroll = appBarLayout.getTotalScrollRange()
+        val percentage: Float = Math.abs(offset) / maxScroll.toFloat()
+        handleToolbarTitleVisibility(percentage)
+    }
+
+    private fun handleToolbarTitleVisibility(percentage: Float) {
+        if (percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+
+            if (!mIsTheTitleVisible) {
+                startAlphaAnimation(binding.titleToolbarDetailNews, ALPHA_ANIMATIONS_DURATION, View.VISIBLE)
+                mIsTheTitleVisible = true
+            }
+
+        } else {
+            if (mIsTheTitleVisible) {
+                startAlphaAnimation(binding.titleToolbarDetailNews, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE)
+                mIsTheTitleVisible = false
+            }
+        }
+    }
+
+    private fun startAlphaAnimation(v: View, duration: Long, visibility: Int) {
+        val alphaAnimation = if (visibility == View.VISIBLE)
+            AlphaAnimation(0f, 1f)
+        else
+            AlphaAnimation(1f, 0f)
+
+        alphaAnimation.duration = duration
+        alphaAnimation.fillAfter = true
+        v.startAnimation(alphaAnimation)
     }
 }
